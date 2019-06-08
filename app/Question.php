100033296c1cf30ec52330bb4e058c0e10e6bb43
Michael Nkanika <mnkanika@AFENETDOM.local>
2019-06-08 14:39:53 +0300

    lazy loading

--- app/Question.php
+++ app/Question.php
@@ -11,11 +11,21 @@ class Question extends Model
     public function user() {
         return $this->belongsTo(User::class);
     }
-    
+
     public function setTitleAttributes($value)
     {
         $this->attributes['title'] = $value;
         $this->attributes['slug'] = str_slug($value, '-');
 
+    }
+
+    public function getUrlAttribute()
+    {
+        return route("questions.show", $this->id);
+    }
+
+    public function getCreatedDateAttribute()
+    {
+        return $this->created_at->diffForHumans();
     }
 }
--- app/User.php
+++ app/User.php
@@ -44,5 +44,9 @@ class User extends Authenticatable
         return $this->hasMany(Question::class);
     }
 
-   
+    public function getUrlAttribute()
+    {
+        //return route("qustion.show", $this->id);
+        return '#';
+    }
 }
