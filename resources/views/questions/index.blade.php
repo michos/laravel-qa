abb1ead44b457d7d1c2ac7b3332e4b6f1ce031ab
Michael Nkanika <mnkanika@AFENETDOM.local>
2019-06-10 12:36:32 +0300

    delete tut, lesson 10

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -48,6 +48,11 @@
                                             </h3>
                                             <div class="ml-auto">
                                             <a href="{{ route('questions.edit', $question->id)}}" class="btn btn-sm btn-outline-info">Edit</a>
+                                            <form class="form-delete" action="{{ route('questions.destroy', $question->id)}}" method="post">
+                                                @method('DELETE')
+                                                @csrf
+                                                <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you Sure?')">Delete</button>
+                                            </form>
                                             </div>
                                 </div>
 
